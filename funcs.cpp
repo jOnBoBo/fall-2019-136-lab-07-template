4d5899f5983882f664a032b577c50d768ca6c1ef
Mike Zamansky <zamansky@gmail.com>
2019-10-20 07:40:14 -0400

    Initial commit

---
+++ funcs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int fact(int n){
+  int p=1;
+  for (int i = 1; i <= n; ++i)
+    {
+      p = p * i;
+    }
+  return p;
+  
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
+#include "funcs.h"
+
+int main(int argc, char *argv[])
+{
+  // your code here
+  return 0;
+}
